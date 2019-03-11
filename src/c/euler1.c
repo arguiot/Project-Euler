8156a31481a2338f9e1800313ed6a9fe5d7a1268
Luka Lafaye de Micheaux <lafayedemicheaux.luka@gmail.com>
2019-03-11 16:38:10 +1100

    Update euler1.c

--- src/c/euler1.c
+++ src/c/euler1.c
@@ -1,24 +1,25 @@
 // Project 1 - Find the sum of all the multiples of 3 or 5 below 1000
 
+// Use the sum function to print the sum of multiples of a number contained by a bigger number n ; sum(number multiple, bigger number n)
+
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
+#include <time.h>
 
-double getSum (int maximum);
-
-int main (int argc, char *argv[]) {
-	int sum = getSum(1000);
-	printf("%d\n", sum); 
-	return EXIT_SUCCESS;
+double sum(double multiples, double a) {
+  return multiples * floor((a - 1.0) / multiples) * (floor((a - 1.0) / multiples) + 1.0) / 2.0;
 }
 
-double getSum (int maximum) {
-	int index = 0;
-	int sum = 0;
-	while (index < maximum) {
-		if (((index % 3) == 0) || ((index % 5) == 0)) {
-			sum = sum + index;
-		}
-		index++;
-	}
-	return sum;
+int main (int argc, char *argv[]) {
+  clock_t begin = clock();
+  
+  double res1 = sum(3.0, 1000.0) + sum(5.0, 1000.0) - sum(15.0, 1000.0);
+  
+  clock_t end = clock();
+  double time_spent = (double)(end - begin) / CLOCKS_PER_SEC;
+  
+  printf("Answer for problem 1 is : \n%f \nDone with repl.it in %f", res1, time_spent);
+
+  return EXIT_SUCCESS;
 }
